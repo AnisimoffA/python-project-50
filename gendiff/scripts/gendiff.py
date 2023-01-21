6cdb7f4c5e3208f8fa202b907d699d82672d613d
Anisimoff <happyprooo@gmail.com>
2023-01-22 00:51:45 +0300

    venv + init + gitignore + gendiff script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # позиционные
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
