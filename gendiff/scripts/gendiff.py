b69f21286094cf6cd57601d7b3648311d0fa0b8e
Anisimoff <happyprooo@gmail.com>
2023-01-24 19:18:58 +0300

    linter

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser()
 
     # позиционные
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    
-    # опциональные
-    parser.add_argument("-f", "--format", help="set format of output", default="JSON")
 
-    args = parser.parse_args()
+    # опциональные
+    parser.add_argument("-f", "--format", help="set format of output", default="JSON")  # noqa: E501
+
+    # args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
