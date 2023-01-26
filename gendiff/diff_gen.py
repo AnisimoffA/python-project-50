a66d3f98e4285e91bb09b0cc93a890de4539d841
Anisimoff <happyprooo@gmail.com>
2023-01-26 16:32:28 +0300

    step3

---
+++ gendiff/diff_gen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import json
+
+
+def generate_diff(file1, file2):
+    file1 = json.load(open(f"tests/fixtures/{file1}"))
+    file2 = json.load(open(f"tests/fixtures/{file2}"))
+
+    difference_list = []
+
+    for k, v in file1.items():
+        if k in file2.keys() and v in file2.values():
+            file2.pop(k)
+            difference_list.append(["   ", k + ":", v])
+            continue
+
+        if k not in file2.keys():
+            difference_list.append(["  -", k + ":", v])
+            continue
+
+        if k in file2.keys() and v not in file2.values():
+            difference_list.append(["  -", k + ":", v])
+            difference_list.append(["  +", k + ":", file2[k]])
+            file2.pop(k)
+            continue
+
+    for k, v in file2.items():
+        difference_list.append(["  +", k + ":", v])
+
+    sort_list = sorted(difference_list, key=lambda x: x[1])
+    new = list(map(
+            lambda x: [x[0], x[1], str(x[2]).lower() if type(x[2]) == bool else str(x[2])]
+                , sort_list))
+    answer_list = "\n".join(["{"] + [" ".join(x) for x in new] + ["}"])
+    
+    print(answer_list)
+    
+def main():
+    generate_diff("file1.json", "file2.json")
+        
+if __name__ == "__main__":
+    main()
+    
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.diff_gen import generate_diff
 
 
 def main():
@@ -12,7 +13,10 @@ def main():
     # опциональные
     parser.add_argument("-f", "--format", help="set format of output", default="JSON")  # noqa: E501
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
+    
+    generate_diff(args.first_file, args.second_file)
+
 
 
 if __name__ == "__main__":
