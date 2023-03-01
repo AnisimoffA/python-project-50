3f16bd35da0547626fa8cfd6a4ede7d6fdbce148
Anisimoff <happyprooo@gmail.com>
2023-03-01 13:49:39 +0300

    lint fix

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,32 +2,32 @@ import itertools
 from gendiff.interface import none_to_null
 
 
-def to_stylish(value, replacer='    ', depth=1):
+def to_stylish(value, replacer='    ', depth=1):  # noqa C901
     value = none_to_null(value)
 
     def iter_(current_value, depth):
         new_depth = depth + 1
-        actual_space = replacer * depth
-        space_for_last_string = (depth-1) * replacer
+        space = replacer * depth
+        space_for_last_string = (depth - 1) * replacer
         lines = []
 
         for value in current_value:
             if value["status"] == "changed":
                 format_old = formatted_value(value.get('old_value'), new_depth)
                 format_new = formatted_value(value.get('new_value'), new_depth)
-                lines.append(f"{actual_space[:-2]}- {value['key']}: {format_old}")
-                lines.append(f"{actual_space[:-2]}+ {value['key']}: {format_new}")
+                lines.append(f"{space[:-2]}- {value['key']}: {format_old}")
+                lines.append(f"{space[:-2]}+ {value['key']}: {format_new}")
 
             if value["status"] == "nested":
-                lines.append(f"{actual_space}{value['key']}: {formatted_value(value.get('changes'), new_depth)}")
+                lines.append(f"{space}{value['key']}: {formatted_value(value.get('changes'), new_depth)}")  # noqa E501
 
             if value["status"] == "same":
-                lines.append(f"{actual_space}{value['key']}: {formatted_value(value.get('changes'), new_depth)}")
+                lines.append(f"{space}{value['key']}: {formatted_value(value.get('changes'), new_depth)}")  # noqa E501
 
             if value["status"] == "added" or value["status"] == "removed":
                 mark = "+" if value["status"] == "added" else "-"
                 format_value = formatted_value(value.get('changes'), new_depth)
-                lines.append(f"{actual_space[:-2]}{mark} {value['key']}: {format_value}")
+                lines.append(f"{space[:-2]}{mark} {value['key']}: {format_value}")  # noqa E501
 
         result = itertools.chain("{", lines, [space_for_last_string + "}"])
         return '\n'.join(result)
@@ -36,15 +36,15 @@ def to_stylish(value, replacer='    ', depth=1):
 
 def formatted_value(data, depth):
     replacer = '    '
-    actual_space = replacer * depth
-    space_for_last_string = (depth-1) * replacer
+    space = replacer * depth
+    space_for_last_string = (depth - 1) * replacer
     lines = []
 
     if isinstance(data, int) or isinstance(data, str):
         return data
     if isinstance(data, dict):
         for key, val in data.items():
-            lines.append(f'{actual_space}{key}: {formatted_value(val, depth+1)}')
+            lines.append(f'{space}{key}: {formatted_value(val, depth+1)}')
         result = itertools.chain("{", lines, [space_for_last_string + "}"])
         return '\n'.join(result)
     if isinstance(data, list):
